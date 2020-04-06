7862025279b4a8ef5e050096d511a2efb2ab9c70
Kevin A. Li <kevinli22527@yahoo.com>
2020-04-06 16:29:00 -0500

    quiz2 subsetSum

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <cstdio>
+
+uint8_t subsetSum(uint32_t array[], uint32_t n, uint32_t k, uint32_t v)
+{
+    if(k == 0 && v == 0)
+    {
+        return true;
+    }
+    else if(k == 0)
+    {
+        return false;
+    }
+    else if (n == 0)
+    {
+        return false;
+    }
+    uint8_t tempRes = 0;
+    if(array[0] <= v)
+    {
+        tempRes = subsetSum(&array[1], n-1, k-1, v-array[0]);
+    }
+    uint8_t tempRes2 = subsetSum(&array[1], n-1, k, v);
+    if(tempRes + tempRes2 > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main() {
+    uint32_t array[] = {1, 8, 6, 4, 3};
+    printf("%d", subsetSum(array, 5, 3, 16));
+    return 0;
+}
